4e9db7a857b89802aa796c959805c04883d44aba
Levi-Batkin <90899332+Levi-Batkin@users.noreply.github.com>
2021-09-17 15:27:53 +0100

    lol

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// sleep(1) = System.Threading.Thread.Sleep(1000)
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D rb;
-
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,17 +20,36 @@ public class Movement : MonoBehaviour
         DoMove();
         
     }
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        if (velocity.x > 0)
+        {
+            transform.localScale = new Vector3(2f, 2f, 1f);
+        }else if (velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-2f, 2f, 1f);
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
 
     void DoJump()
     {
         Vector2 velocity = rb.velocity;
 
         // check for jump
-        if (Input.GetKeyDown("w") )
+        if ((Input.GetKeyDown("w") || Input.GetKeyDown("space") || Input.GetKeyDown("up")) && (isGrounded == true))
         {
             if (velocity.y < 0.01f )
             {
-                velocity.y = 5f;    // give the player a velocity of 1.5 in the y axis
+                velocity.y = 7f;    // give the player a velocity of something in the y axis
                 
             }
         }
@@ -46,13 +66,13 @@ public class Movement : MonoBehaviour
         velocity.x = 0;
 
         // check for moving left
-        if ( Input.GetKey("a"))
+        if (Input.GetKey("a") || Input.GetKey("left"))
         {
             velocity.x = -5f;
         }
 
         // check for moving right
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") || Input.GetKey("right"))
         {
             velocity.x = 5f;
         }
